f73a3d1c2c12b24bb7d01ed9697b369c54747db4
endcore <endiguns1997@gmail.com>
2026-06-30 05:52:07 +0700

    Refactor click listener and code formatting

--- app/src/main/java/com/draco/ladb/views/MainActivity.kt
+++ app/src/main/java/com/draco/ladb/views/MainActivity.kt
@@ -183,32 +183,32 @@ class MainActivity : AppCompatActivity() {
             .create()
             .apply {
                 setOnShowListener {
-		    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
 
-    val port = DnsDiscover.adbPort?.toString()
+                        val port = DnsDiscover.adbPort?.toString()
 
-    if (port == null) {
-        Snackbar.make(
-            binding.output,
-            "Searching Wireless Debugging port...",
-            Snackbar.LENGTH_SHORT
-        ).show()
-        return@setOnClickListener
-    }
+                        if (port == null) {
+                            Snackbar.make(
+                                binding.output,
+                                "Searching Wireless Debugging port...",
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                            return@setOnClickListener
+                        }
 
-    val code =
-        findViewById<TextInputEditText>(R.id.code)!!
-            .text
-            .toString()
+                        val code =
+                            findViewById<TextInputEditText>(R.id.code)!!
+                                .text
+                                .toString()
 
-    dismiss()
+                        dismiss()
 
-    lifecycleScope.launch(Dispatchers.IO) {
-        viewModel.adb.debug("Trying to pair on port $port...")
-        val success = viewModel.adb.pair(port, code)
-        callback?.invoke(success)
-    }
-}
+                        lifecycleScope.launch(Dispatchers.IO) {
+                            viewModel.adb.debug("Trying to pair on port $port...")
+                            val success = viewModel.adb.pair(port, code)
+                            callback?.invoke(success)
+                        }
+                    }
 
                     getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener {
                         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.tutorial_url)))
